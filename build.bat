c088cedbcef2acce88c94ac610986bfa56ab3341
Vladimir Minacov <Vladimir.Minacov@endava.com>
2022-06-07 15:45:17 +0300

    Launch k8s with 4 nodes

--- build.bat
+++ build.bat
@@ -8,7 +8,9 @@ docker build -t membership -f Dockerfile-app .
 kubectl delete deployment membership1-deployment
 kubectl delete deployment membership2-deployment
 kubectl delete deployment membership3-deployment
+kubectl delete deployment membership4-deployment
 
 kubectl apply -f chart\membership1-deployment.yaml
 kubectl apply -f chart\membership2-deployment.yaml
 kubectl apply -f chart\membership3-deployment.yaml
+kubectl apply -f chart\membership4-deployment.yaml
